600dba5799a03ea29235f220e5ec969e960c99cd
micuat <micuat@gmail.com>
2019-05-17 16:27:20 +0000

    send serial response to osc

--- raspi_node/index.js
+++ raspi_node/index.js
@@ -6,14 +6,19 @@ const parser = new Readline()
 
 
 const { Server } = require('node-osc');
+const { Client } = require('node-osc');
+const hostname = "192.168.1.140";
 const oscServer = new Server(3333, '0.0.0.0');
+const oscClient = new Client(hostname, 3334);
 
+let isSent = false;
 oscServer.on('message', function (msg) {
   const address = msg[0];
   if(address == '/oco/command') {
     const command = msg[1];
     console.log(`execute: ${command}`);
     port.write(`${command}\n`);
+    isSent = true;
   }
 });
 
@@ -26,5 +31,10 @@ parser.on('data', line => {
   }
   else {
     console.log(`\n> ${line}`)
+    if(isSent) {
+      oscClient.send("/oco/response", line, () => {
+        isSent = false;
+      });
+    }
   }
 })
