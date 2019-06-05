80acb7ff9169338e3ea44f15508d6128ccd9c09b
micuat <micuat@gmail.com>
2019-06-05 16:52:55 +0200

    trigger

--- raspi_brain/index.js
+++ raspi_brain/index.js
@@ -6,6 +6,39 @@ const button = new Gpio(6, {
   edge: Gpio.EITHER_EDGE
 });
 
+
+class BumperManager {
+  constructor () {
+    this.isOnWall = false;
+    this.lastTime = this.getMillis();
+    this.hitThreshold = 100;
+  }
+
+  getMillis () {
+    return Date.now();
+  }
+
+  interrupt (level) {
+    const t = this.getMillis();
+    if(level == '1' && this.isOnWall == false) {
+      if(t - this.lastTime < this.hitThreshold) {
+        console.log('misdetection');
+      }
+      else {
+        console.log('hit');
+        this.isOnWall = true;
+        this.lastTime = t;
+      }
+    }
+    else if(level == '0' && this.isOnWall == true) {
+      console.log('released');
+      this.isOnWall = false;
+    }
+  }
+}
+
+const bm = new BumperManager();
+
 button.on('interrupt', (level) => {
-  console.log(level)
+  bm.interrupt(level);
 });
