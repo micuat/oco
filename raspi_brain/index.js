cf2fa354eb34b5bb46afc2fdc5da02ff5f2f1e16
micuat <micuat@gmail.com>
2019-06-16 09:16:40 +0200

    cleanup

--- raspi_brain/index.js
+++ raspi_brain/index.js
@@ -19,57 +19,6 @@ http.listen(settings.httpPort, () => {
   console.log('listening on *:' + settings.httpPort);
 });
 
-class Point {
-  constructor() {
-    this.x = 0;
-    this.y = 0;
-  }
-  add(p0) {
-    const p = new Point();
-    p.x = this.x + p0.x;
-    p.y = this.y + p0.y;
-    return p;
-  }
-}
-
-class World {
-  constructor() {
-    this.relative = new Point();
-    this.origin = new Point();
-    this.angle = 0;
-  }
-
-  clearX() {
-  }
-  clearY() {
-    this.origin.x += this.getRotatedRelative().x;
-    this.origin.y += this.getRotatedRelative().y;
-    this.relative.x = 0;
-    this.relative.y = 0;
-  }
-  clearZ() {
-  }
-  moveToA(x, y) {
-    this.relative.x = x;
-    this.relative.y = y;
-  }
-  rotate(t) {
-    this.angle += t;
-  }
-  getRotatedRelative() {
-    const p = new Point();
-    let t = this.angle * Math.PI / 180;
-    p.x = Math.cos(t) * this.relative.x - Math.sin(t) * this.relative.y;
-    p.y = Math.sin(t) * this.relative.x + Math.cos(t) * this.relative.y;
-    return p;
-  }
-  getPosition() {
-    return this.origin.add(this.getRotatedRelative());
-  }
-}
-
-const world = new World();
-
 class CommandQueue {
   constructor() {
     this.queue = [];
@@ -86,6 +35,36 @@ class CommandQueue {
     this.servoDelay = 1000;
     this.hit = false;
     this.driveDelay = 100;
+
+    this.execCommand = {
+      home: (command) => {
+        this.send('home');
+      },
+      clearX: (command) => {
+        this.send('clearX');
+      },
+      clearY: (command) => {
+        this.send('clearY');
+      },
+      clearZ: (command) => {
+        this.send('clearZ');
+      },
+      moveToA: (command) => {
+        this.send(`moveToA ${command[1]} ${command[2]} ${command[2]} ${this.driveDelay}`);
+      },
+      drive: (command) => {
+        this.send(`drive ${command[1]} ${command[2]} ${command[2]} ${this.driveDelay}`);
+      },
+      driveTillHit: (command) => {
+        this.send(`driveTillHit ${this.driveDelay}`);
+      },
+      rotate: (command) => {
+        this.send(`drive 0 ${command[1] * 1800} -${command[1] * 1800} ${this.driveDelay}`);
+      },
+      servo: (command) => {
+        this.send(`servo ${command[1]} ${command[2]} ${command[3]}`);
+      },
+    }
   }
   add(m) {
     this.queue.push(m);
@@ -163,44 +142,7 @@ class CommandQueue {
   next() {
     if (this.isMessageSendable()) {
       const command = this.pop();
-      if (command[0] == 'home') {
-        world.clearX();
-        this.send('home');
-      }
-      if (command[0] == 'clearX') {
-        world.clearX();
-        this.send('clearX');
-      }
-      if (command[0] == 'clearY') {
-        world.clearY();
-        this.send('clearY');
-      }
-      if (command[0] == 'clearZ') {
-        world.clearZ();
-        this.send('clearZ');
-      }
-      if (command[0] == 'moveToA') {
-        world.moveToA(command[1], command[2]);
-        this.send(`moveToA ${command[1]} ${command[2]} ${command[2]} ${this.driveDelay}`);
-      }
-      if (command[0] == 'drive') {
-        world.moveToA(command[1], command[2]);
-        this.send(`drive ${command[1]} ${command[2]} ${command[2]} ${this.driveDelay}`);
-      }
-      if (command[0] == 'driveTillHit') {
-        world.moveToA(command[1], command[2]);
-        this.send(`driveTillHit ${this.driveDelay}`);
-      }
-      if (command[0] == 'rotate') {
-        world.rotate(command[1]);
-        this.send(`drive 0 ${command[1] * 1800} -${command[1] * 1800} ${this.driveDelay}`);
-      }
-      if (command[0] == 'servo') {
-        io.emit('servo', { angle: command[1] });
-        this.send(`servo ${command[1]} ${command[2]} ${command[3]}`);
-      }
-      const p = world.getPosition();
-      io.emit('world', { x: p.x, y: p.y });
+      this.execCommand[command[0]](command);
       this.messageJustSent();
     }
     if (this.isEmpty() && this.driveTillHitFlag) {
